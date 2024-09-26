ae72c4e2ab31fdd7f236fefe1b0e29455e152638
kitisak.c <kitisak.chalongjan@siamsmartsolutions.com>
2024-09-26 16:52:17 +0700

    edit text file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,8 +13,5 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const FlutterApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('Flutter App'), findsOneWidget);
   });
 }
